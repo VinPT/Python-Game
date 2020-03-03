af9b961b1c90a600cd2c4dd8bc49cd28d1372466
Vinayaka Patrick Thompson <vinayaka.p.thompson@live.com>
2020-03-02 21:01:17 -0800

    made next room generator

--- LevelGenerator.py
+++ LevelGenerator.py
@@ -9,34 +9,61 @@ class LevelGenerator:
     def __init__(self, xSize, ySize): #zSize not included thus keeping it two dimentional for now.
         
         self.generateNewMap(xSize,ySize,1)
-        
-        
+            
     
     def generateNewMap(self, xSize,ySize,Zsize):
         self.level = [[TempRoom for x in range(xSize)] for x in range(ySize)]
         
-        print("Hello my size is " , len(self.level), len(self.level[0]))
-        self.makeRoom("null","1f", 0, 0)
+        print("Hello my size is " , len(self.level), len(self.level[0]))       #i think it is Y X this might get me later 
+        self.makeRoom(None,"1f", 0, 0)
 
     def makeRoom(self, lastRoom, roomName , locx, locy):
-        deepest = "null"
+        deepestNode = None
         depth = 0
+        choices = ["L","F","R"]
         roomExitCalculator = Probability([30,55,15])
         numberExits = roomExitCalculator.calculate()
         
 
-        if self.level[locx][locy].roomName == "null":
+        if self.level[locx][locy].roomName == "none":
             self.level[locx][locy].roomName = roomName
             
             #generate Exits
             #if 0 is returned for depth dont consider room generated
+            print("number exits", numberExits)
 
-
+            for x in range(numberExits):
+                self.makeRoom(self.level[locx][locy], "2F", 1, 1)
 
         else:
-            deepest = roomName
+            deepestNode = roomName
 
-        return depth, deepest    
+        return depth, deepestNode    
+
+    def _nextRoomDataGenerator(self, lastRoom,thisRoom,roomLetter):
+        name = "neyt"
+        locx = 0
+        locy = 0
+        xdif = thisRoom.xloc - lastRoom.xloc
+        ydif = thisRoom.yloc - lastRoom.yloc
+
+        if (roomLetter == "F"):
+            locx = thisRoom.xloc + xdif
+            locy = thisRoom.yloc + ydif
+
+        elif (roomLetter == "L"):
+            locx = thisRoom.xloc + ydif
+            locy = thisRoom.yloc + xdif
+    
+        elif (roomLetter == "R"):
+            locx = thisRoom.xloc - ydif
+            locy = thisRoom.yloc - xdif
+        
+
+        if (locx < 0 or locx < 0 or locx >= len(self.level)  or locy >= len(self.level[0])):
+            return None
+        else:
+            return name, thisRoom.locx, thisRoom.locy
         
 
 
@@ -64,7 +91,7 @@ class Probability:
             threshhold = threshhold + self.probabilityArr[x]
 
             if threshhold > random:
-                result = x
+                result = x + 1
                 break
 
         return result
@@ -72,13 +99,16 @@ class Probability:
         
         
 class TempRoom:
-    roomName = "null";
+    roomName = "none";
+    depth = 0
+    xloc = 0
+    yloc = 0
     connectedRooms = [];
 
     
     
 p1 = LevelGenerator(10,10)
-p1.generateNewMap(1,1,1)
+#p1.generateNewMap(1,1,1)
 
 
 1 # 60%
